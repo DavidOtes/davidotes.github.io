4c13a94e1dd215af45195381c50d1dcd7fa0023a
DavidOtes <72026456+DavidOtes@users.noreply.github.com>
2023-09-10 23:18:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function updateTimeandDate() {
+    const dateUTC = document.getElementById("date");
+    const timeUTC = document.getElementById("time");
+
+    const presentDate = new Date();
+
+
+    const dateOptions = {
+        timeZone: 'UTC',
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+
+    const DateFormat = presentDate.toLocaleDateString(undefined, dateOptions);
+    dateUTC.textContent = `Date (UTC): ${DateFormat}`;
+
+    const timeOptions = {
+        timeZone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        fractionalSecondDigits: 3,
+    };
+
+    const TimeFormat = presentDate.toLocaleTimeString(undefined, timeOptions);
+    timeUTC.textContent = `Time (UTC): ${TimeFormat}`;
+}
+
+function initializePage() {
+    updateTimeandDate();
+
+    setInterval(updateTimeandDate, 10);
+}
+
+window.addEventListener("load", initializePage);
+
